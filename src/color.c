807064346548109a636e2200031fdaa771e8b4b5
Martin Eyben <martin.eyben@enseirb-matmeca.fr>
2023-11-07 14:51:48 +0100

    add color.c

---
+++ src/color.c
@@ -0,0 +1,40 @@
+#include "color.h"
+
+const char * str_color = {
+  "Black",
+  "Blue",
+  "Green",
+  "Red",
+  "White",
+  "Gold",
+  "Pearl",
+  "Aquamarine",
+  "Octarine",
+  "Chestnut"
+};
+
+const char * str_short_color = {
+  "K",
+  "B",
+  "G",
+  "R",
+  "W",
+  "D",
+  "P",
+  "A",
+  "O",
+  "C"
+};
+
+const char* color_to_string(enum color_t c)
+{
+  return str_color[i];
+  
+}
+
+const char* color_to_short(enum color_t c)
+{
+  return str_short_color[i];
+}
+  
+  
