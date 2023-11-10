e4eb17bab18567d12b8cbfe9d564c907903000e6
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2023-11-10 17:22:37 +0100

    Finished builders' tests

--- tst/test_builders/test_builders.h
+++ tst/test_builders/test_builders.h
@@ -2,7 +2,10 @@
 #define __TEST_BUILDERS_H__
 
 #include <stdio.h>
+#include <time.h>
+
 #include "builder.h"
+#include "builder_constants.h"
 
 
 #define SEED 0
@@ -13,7 +16,7 @@ int test_builders();
 
 
 // Tests if init is executable and create all builders
-int test_init_builders();
+int test_init_builders(int seed);
 
 
 /*
@@ -22,10 +25,18 @@ int test_init_builders();
  *
  * Notes: it executes init_builders
  *
- * Returns 1 if runs well, 0 otherwise
+ * Returns 1 if runs as expected, 0 otherwise
  */
-int test_builders_levels();
+int test_builders_levels(int seed);
 
+/*
+ * Tests if builder_pts() returns a legal amount of pts
+ *
+ * Notes: it executes init_builders
+ *
+ * Returns 1 if runs as expected, 0 otherwise
+ */
+int test_builders_pts(int seed);
 
 /*
  * Tests if builder_level() is executable and if the returned level
@@ -33,8 +44,18 @@ int test_builders_levels();
  *
  * Notes: it executes init_builders
  *
- * Returns 1 if runs well, 0 otherwise
+ * Returns 1 if runs as expected, 0 otherwise
  */
-int test_builders_requires();
+int test_builders_requires(int seed);
+
+/*
+ * Tests if builder_level() is executable and if the returned level
+ * is legal
+ *
+ * Notes: it executes init_builders
+ *
+ * Returns 1 if runs as expected, 0 otherwise
+ */
+int test_builders_provides(int seed);
 
 #endif
