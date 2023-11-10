7476f6a95205d26f0c06ccdecc01a343f3d9a53e
Martin Eyben <martin.eyben@enseirb-matmeca.fr>
2023-11-10 14:41:06 +0100

    add player.h

---
+++ src/players/players.h
@@ -0,0 +1,13 @@
+#ifndef __PLAYERS_H__
+#define __PLAYERS_H__
+
+struct player_t;
+
+void player_take_token(struct *player_t player, unsigned int index);
+void player_hire_builder(struct *player_t player, unsigned int index );
+void player_display_inventory(struct *player_t player);
+
+
+
+
+#endif
