cd29562c631539ba62b395d132050da527a27968
Martin Eyben <martin.eyben@enseirb-matmeca.fr>
2023-11-11 22:20:00 +0100

    add builder points to the display function

--- src/builders/builder.c
+++ src/builders/builder.c
@@ -36,13 +36,13 @@ void init_builders(unsigned int seed)
 	for (int i = 0 ; i < n_builders ; ++i)
 	{
 		builders[i].lvl = rand() % NUM_LEVELS;
-		builders[i].pts = BUILDER_MIN_PTS + rand() % (BUILDER_MAX_PTS - BUILDER_MIN_PTS);
+		builders[i].pts = BUILDER_MIN_PTS + rand() % (BUILDER_MAX_PTS - BUILDER_MIN_PTS +1);
 
 		builders[i].provides.c = rand() % NUM_COLORS;
-		builders[i].provides.n = BUILDER_MIN_PROVIDES + rand() % (BUILDER_MAX_PROVIDES - BUILDER_MIN_PROVIDES);
+		builders[i].provides.n = BUILDER_MIN_PROVIDES + rand() % (BUILDER_MAX_PROVIDES - BUILDER_MIN_PROVIDES + 1);
 
 		builders[i].requires.c = rand() % NUM_COLORS;
-		builders[i].requires.n = BUILDER_MIN_COST +  rand() % (BUILDER_MAX_COST - BUILDER_MIN_COST);
+		builders[i].requires.n = BUILDER_MIN_COST +  rand() % (BUILDER_MAX_COST - BUILDER_MIN_COST + 1);
 
 	}
 }
@@ -89,13 +89,14 @@ struct buildcost_t builder_provides(const struct builder_t *g)
 
 void builder_display(const struct builder_t *g, const char *prefix)
 {
-	printf("%sBuilder(lvl=%d,cost=%d%s,prod=%d%s)\n", \
+	printf("%sBuilder(lvl=%d,cost=%d%s,prod=%d%s,points=%d)\n", \
 			prefix, \
 			g->lvl, \
 			g->requires.n, \
 			color_to_short_string(g->requires.c), \
 			g->provides.n,
-			color_to_short_string(g->provides.c)
+			color_to_short_string(g->provides.c),
+			builder_points(g)
 			);
 }
 
