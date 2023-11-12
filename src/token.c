38fe152d75847b48d3b68990c2d5aad76a6b374f
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2023-11-12 12:38:11 +0100

    Typo

--- src/token.c
+++ src/token.c
@@ -15,21 +15,22 @@ struct token_t create_complex_token(unsigned int c[NUM_COLORS])
 {
   struct token_t new_token = {};
   for (int i = 0; i < NUM_COLORS; ++i)
-    {
-      new_token.c[i]=c[i];
-    }
+	{
+		new_token.c[i]=c[i];
+	}
   return new_token;
 }
 
 int token_equals(const struct token_t t1, const struct token_t t2)
 {
   for (unsigned int index = 0; index < NUM_COLORS; ++index)
-    {
-      if (t1.c[index] != t2.c[index])
 	{
-	  return 0;
+		if (t1.c[index] != t2.c[index])
+		{
+			return 0;
+		}
 	}
-    }
+
   return 1;
 }
 
@@ -38,7 +39,7 @@ void token_display(struct token_t t, const char* prefix)
 	printf("%sTOKEN(", prefix);
 	for (int i = 0 ; i < NUM_COLORS ; ++i)
 	{
-		if(t.c[i] != 0)
+		if (t.c[i] != 0)
 		{
 			printf("%s=%d (%d),", color_to_short_string(i), i, t.c[i]);	  
 		}
