0c99bb6f105bd4546ad72008e7fdd2642ba7d8d6
Martin Eyben <martin.eyben@enseirb-matmeca.fr>
2023-11-15 21:27:50 +0100

    add test_player functions

--- tst/test_players/test_players.c
+++ tst/test_players/test_players.c
@@ -1,11 +1,23 @@
 #include "test_players.h"
+#include "builder.h"
+#include "guild.h"
+#include "market.h"
 #include "players.h"
 #include "token.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include "ansi_color.h"
 
 int test_players()
 {
+    /*
+        Init all needed instances
+    */
+    srand(time(NULL));
+    init_market(rand());
+    init_builders(rand());
+
+
     int test_passed = 0 ;
     if(test_init_players())
     {
@@ -43,7 +55,7 @@ int test_init_players()
     struct player_t new_player = init_player();
     if(new_player.current_point != 0)
     {
-        fprintf(stderr,"test_init_players : Wrong initial point for player\n");
+        fprintf(stderr,RED"test_init_players : Wrong initial point for player\n" CRESET);
         return 0;
     }
 
@@ -51,7 +63,7 @@ int test_init_players()
     {
         if (new_player.ressources.builders[index]) 
         {
-            fprintf(stderr,"test_init_players : Player has initial builders in his inventory\n");
+            fprintf(stderr,RED "test_init_players : Player has initial builders in his inventory\n" CRESET);
             return 0;
         }
     }
@@ -60,7 +72,7 @@ int test_init_players()
     {
         if (new_player.ressources.tokens[index]) 
         {
-            fprintf(stderr,"Player has initial tokens in his inventory\n");
+            fprintf(stderr,RED "Player has initial tokens in his inventory\n" CRESET);
             return 0;
         }
     }
@@ -72,11 +84,25 @@ int test_hire_builder()
     // make sure the players are init and does it well
 	if (!test_init_players())
 	{
-		fprintf(stderr, "test_hire_builder: test_init_players didn't run successfully\n");
+		fprintf(stderr, RED "test_hire_builder: test_init_players didn't run successfully\n" CRESET);
 		return 0;
 	}
+    srand(time(NULL));
+    struct player_t new_player = init_player();
 
-    //struct player_t new_player = init_player();
+    unsigned int hired_builder = rand() % num_builders();
+    player_hire_builder(&new_player, hired_builder);
+
+    if (guild_is_available(hired_builder))
+    {
+        fprintf(stderr, RED "test_hire_builder: hired builder is still available in guild\n" CRESET);
+		return 0;
+    }
+    if(!new_player.ressources.builders[hired_builder])
+    {
+        fprintf(stderr, RED "test_hire_builder: hired builder don't belong to the player\n" CRESET);
+		return 0;
+    }
     return 1;
 
     
@@ -84,16 +110,73 @@ int test_hire_builder()
 
 int test_player_pick_token()
 {
-    return 1;
+    if (!test_init_players())
+	{
+		fprintf(stderr, RED "test_player_pick_token: test_init_players didn't run successfully\n" CRESET);
+		return 0;
+	}
+    struct player_t new_player = init_player();
+    struct token_t* picked_token = get_token(0); //the picked token need to be the first available
+    player_pick_token(&new_player);
+
+    /*
+        Check if there is a new token in player inventory and no longer in the market
+    */
+
+    for (int index = 0; index < NUM_TOKENS; ++index)
+    {
+        if(new_player.ressources.tokens[index]) // if a token is in player inventory
+        {
+            if( picked_token != new_player.ressources.tokens[index])
+            {
+                fprintf(stderr, RED "test_player_pick_token: wrong token picked\n" CRESET);
+                return 0;
+            }
+            return 1;
+        }
+    }
+    fprintf(stderr, RED "test_player_pick_token: no token available in player inventory\n" CRESET);
+    return 0;
 }
 
 int test_player_take_token()
 {
+    if (!test_init_players())
+	{
+		fprintf(stderr, RED "test_player_take_token: test_init_players didn't run successfully\n" CRESET);
+		return 0;
+	}
+    if (!test_player_pick_token())
+	{
+		fprintf(stderr, RED "test_player_take_token: test_player_pick_token didn't run successfully\n" CRESET);
+		return 0;
+	}
+    struct player_t new_player = init_player();
+    struct token_t* picked_token = get_token(0); //the picked token need to be the first available
+    player_pick_token(&new_player);
+    player_pick_token(&new_player);
+
+    player_take_token(&new_player, picked_token);
+    for (int index = 0; index < NUM_TOKENS; ++index)
+    {
+        if(new_player.ressources.tokens[index] == picked_token)
+        {
+            fprintf(stderr, RED "test_player_take_token: paid token is still in player's inventory\n" CRESET);
+		    return 0;
+        }
+    }
+
     return 1;
 }
 
 int test_player_pay_builder()
 {
+    if (!test_init_players())
+	{
+		fprintf(stderr, RED "test_player_pay_builder: test_init_players didn't run successfully\n" CRESET);
+		return 0;
+	}
+    struct player_t new_player = init_player();
     return 1;
 }
 
