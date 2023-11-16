aae12b2c26b481b9d04b76857cf9f9cb29efd1b8
Martin Eyben <martin.eyben@enseirb-matmeca.fr>
2023-11-16 21:03:44 +0100

    indentation

--- src/builders/guild.c
+++ src/builders/guild.c
@@ -8,39 +8,38 @@
 static struct guild guild;
 void init_guild()
 {
-    guild.n_builders = num_builders();
-    for (int index = 0; index < guild.n_builders; ++index)
-    {
-        guild.builders[index] = make_builder(index);
-
-				stack_append(&guild.available, &guild.builders[index]);
-    }
+  guild.n_builders = num_builders();
+  for (int index = 0; index < guild.n_builders; ++index)
+  {
+    guild.builders[index] = make_builder(index);
+		stack_append(&guild.available, &guild.builders[index]);
+  }
     
 }
 
 
 int guild_nb_builder()
 {
-    return guild.n_builders;
+  return guild.n_builders;
 }
 
 void guild_display()
 {
-    struct available_builders available_builders = {};
-		available_builders.n_builders_available = stack_get_values(&guild.available, available_builders.available);
+  struct available_builders available_builders = {};
+	available_builders.n_builders_available = stack_get_values(&guild.available, available_builders.available);
 
-    for (unsigned int index = 0 ; index < available_builders.n_builders_available ; ++index)
-    {
-			builder_display(available_builders.available[index], " --- ");
-    }
+  for (unsigned int index = 0 ; index < available_builders.n_builders_available ; ++index)
+  {
+		builder_display(available_builders.available[index], " --- ");
+  }
 }
 
 
 struct builder_t* guild_pick_builder()
 {
-		struct builder_t * builder = stack_pop(&guild.available);
-		guild.n_builders--;
-    return builder;
+	struct builder_t * builder = stack_pop(&guild.available);
+	guild.n_builders--;
+  return builder;
 }
 
 
@@ -52,10 +51,10 @@ void guild_put_builder(struct builder_t * builder)
 
 struct available_builders get_available_builders()
 {
-    struct available_builders available_builders = {};
-		available_builders.n_builders_available = stack_get_values(&guild.available, available_builders.available);
+  struct available_builders available_builders = {};
+	available_builders.n_builders_available = stack_get_values(&guild.available, available_builders.available);
 
-    return available_builders;
+  return available_builders;
 }
 
 // int get_first_available_builder(int i)
--- src/builders/guild.h
+++ src/builders/guild.h
@@ -9,7 +9,7 @@ struct guild
 {
     struct builder_t *builders[MAX_BUILDERS];
     int n_builders;
-		struct stack_t available;
+	struct stack_t available;
 };
 
 
