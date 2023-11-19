089d6dc8a1290b2d2237fdcd9ec639564c1dbf63
Martin Eyben <martin.eyben@enseirb-matmeca.fr>
2023-11-19 23:31:46 +0100

    add display market board functions

---
+++ src/display/market_display.h
@@ -0,0 +1,21 @@
+#ifndef __MARKET_DISPLAY_H__
+#define __MARKET_DISPLAY_H__
+
+#include <stdio.h>
+#include "token.h"
+#include "token_second_header.h"
+#include "math.h"
+#include "ansi_color.h"
+#include <math.h>
+#include <stdio.h>
+
+/*
+    Display the game market board, need to give the list of the token in a matrix
+*/
+void display_board(struct token_t* board[(int) sqrt(NUM_TOKENS)][(int) sqrt(NUM_TOKENS)], char* tags[(int) sqrt(NUM_TOKENS)][(int) sqrt(NUM_TOKENS)]);
+
+/*
+    Place tokens in a matrice board[rac(NUM_TOKENS)][rac(NUM_TOKENS)] and fills the tags with the direction
+*/
+void place_token_in_board(struct token_t *tokens[NUM_TOKENS], struct token_t* board[(int) sqrt(NUM_TOKENS)][(int) sqrt(NUM_TOKENS)], char *tags[(int) sqrt(NUM_TOKENS)][(int) sqrt(NUM_TOKENS)]);
+#endif
--- tst/test.c
+++ tst/test.c
@@ -9,6 +9,8 @@
 
 #include "test_can_buy.h"
 #include "test_players.h"
+#include "token.h"
+#include <math.h>
 #define SEED 0
 
 #include <stdlib.h>
@@ -19,6 +21,9 @@
 #include "test_guild.h"
 #include "test_market.h"
 #include "ansi_color.h"
+#include "market_display.h"
+
+#include "market.h"
 #define _NB_MIN_PARAMS_ 1
 
 
@@ -40,6 +45,15 @@ int main(int argc, char *argv[])
 	test_players();
 	test_utils();
 	
+	init_market(0);
+	
+	market_shuffle();
+	market_display();
+	int board_size = sqrt(NUM_TOKENS);
+	struct token_t* board[board_size][board_size];
+	char * tags[board_size][board_size];
+	place_token_in_board(get_available_tokens()->available, board, tags);
+	display_board(board,tags);
 
 
 
