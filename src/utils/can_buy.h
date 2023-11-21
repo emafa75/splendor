e57b29631735b2556e770a5b62cc3490f62c8311
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2023-11-21 17:40:10 +0100

    Changed can_buy to use new set_t

--- src/utils/can_buy.h
+++ src/utils/can_buy.h
@@ -3,6 +3,7 @@
 
 #include "builder.h"
 
+#include "set.h"
 #include "market.h"
 #include "guild.h"
 #include "token.h"
@@ -18,6 +19,9 @@
  *  If it can't be bought, all the values of the array are set to NULL  
  */
 struct ressources can_buy(struct builder_t *builder, struct ressources ressources);
+
+
+unsigned int is_usable(struct set_t *set, struct set_t cost);
 
 /*
     Select the first affordable builder in the guild, other return -1
