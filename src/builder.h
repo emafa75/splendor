f9a725a5cc28b7db417d157e9b6b13c0b83fde0b
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2023-11-21 16:00:12 +0100

    Get the new headers files

--- src/builder.h
+++ src/builder.h
@@ -2,23 +2,17 @@
 #define __BUILDER_H__
 
 #include "color.h"
+#include "set.h"
 
 /** The maximum level for a builder */
 #ifndef NUM_LEVELS
   #define NUM_LEVELS 2
 #endif
 
-/** The maximum number of builders */
+/** The maximum number of builders in total */
 #ifndef MAX_BUILDERS
   #define MAX_BUILDERS 10
 #endif
-
-/** A pair representing a number of tokens of the same color that is
-    required to buy a builder or that is produced by a builder. */
-struct buildcost_t {
-  enum color_t c; // the color
-  unsigned int n; // the number of tokens
-};
 
 /** Abstract struct representing a builder. A builder is identified by
     its level and its index between 0 and num_builders(level)-1. */
@@ -31,7 +25,7 @@ void init_builders(unsigned int seed);
 /** Returns the total number of builders in the game. */
 unsigned int num_builders();
 
-/** Returns the `index`-th builder and NULL if it does not exist. */
+/** Returns the `index`-th builder, and NULL if it does not exist. */
 struct builder_t* make_builder(unsigned int index);
 
 /** Returns the level of the builder. */
@@ -41,10 +35,10 @@ unsigned int builder_level(const struct builder_t* g);
 unsigned int builder_points(const struct builder_t* g);
 
 /** Returns the cost that must be payed in order to hire this builder. */
-struct buildcost_t builder_requires(const struct builder_t* g);
+struct set_t builder_requires(const struct builder_t* g);
 
 /** Returns the cost of the tokens that can be produced by this builder. */
-struct buildcost_t builder_provides(const struct builder_t* g);
+struct set_t builder_provides(const struct builder_t* g);
 
 /** Displays the builder on the stdout, after a possible prefix.
     Example : builder_display(g, "    - ") displays on the screen :
@@ -53,5 +47,3 @@ struct buildcost_t builder_provides(const struct builder_t* g);
 void builder_display(const struct builder_t* g, const char* prefix);
 
 #endif // __BUILDER_H__
-
-
--- src/token.h
+++ src/token.h
@@ -2,6 +2,7 @@
 #define __TOKEN_H__
 
 #include "color.h"
+#include "set.h"
 
 /** The number of tokens in play. */
 #ifndef NUM_TOKENS
@@ -11,12 +12,12 @@
 /** A type describing a token, that is to say a set of different
     colors. */
 struct token_t {
-  unsigned int c[NUM_COLORS];
+  struct set_t s;
 };
 
 /** Builder for tokens. */
 struct token_t create_simple_token(enum color_t c);
-struct token_t create_complex_token(unsigned int c[NUM_COLORS]);
+struct token_t create_complex_token(struct set_t s);
 
 /** Compares the two tokens `t1` and `t2` and returns a boolean
     telling whether they are equal or different. */
