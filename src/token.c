56b22c629b090859675016dcd92029f2f4373c50
Nemo D'acremont <nemo.dacremont@enseirb-matmeca.fr>
2023-11-21 16:54:17 +0100

    change token

--- src/token.c
+++ src/token.c
@@ -2,56 +2,45 @@
 #include "token.h"
 #include "token_second_header.h"
 
+
 struct token_t create_simple_token(enum color_t c)
 {
   struct token_t new_token={};
-  new_token.c[c]=1;
+	unsigned int values[NUM_COLORS] = {};
+	values[c] = 1;
+
+	new_token.s = set_create(c);
   return new_token;
-  
 }
+
 
 struct token_t create_complex_token(unsigned int c[NUM_COLORS])
 {
   struct token_t new_token = {};
-  for (int i = 0; i < NUM_COLORS; ++i)
-	{
-		new_token.c[i]=c[i];
-	}
+	new_token.s = set_create(c);
   return new_token;
 }
 
+
 int token_equals(const struct token_t t1, const struct token_t t2)
 {
-  for (unsigned int index = 0; index < NUM_COLORS; ++index)
-	{
-		if (t1.c[index] != t2.c[index])
-		{
-			return 0;
-		}
-	}
-
-  return 1;
+	return set_are_equals(&t1.s, &t2.s);
 }
+
 
 void token_display(struct token_t t, const char* prefix)
 {
-	printf("%sTOKEN(", prefix);
-	for (enum color_t i = 0 ; i < NUM_COLORS ; ++i)
-	{
-		if (t.c[i] != 0)
-		{
-			printf("%s%s%s=%d (Q:%d)," ,color_prefix(i), color_to_short_string(i),CRESET, i, t.c[i]);	  
-		}
-	}
-  printf(")\n");
+	set_display(&t.s);
 }
 
+
+// TODO: change t.s.c
 void token_short_diplay(struct token_t t)
 {
 	printf("T(");
 	for (enum color_t i = 0; i < NUM_COLORS; ++i)
 	{
-		if(t.c[i] != 0)
+		if(t.s.c[i] != 0)
 		{	
 			// if(t.c[i]>1 )
 			// {
