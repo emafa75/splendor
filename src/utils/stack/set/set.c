8a54d597a056d76f2e29791e5d44cfc4d05dc47a
Martin Eyben <martin.eyben@enseirb-matmeca.fr>
2023-11-21 16:53:42 +0100

    added set_t functions

--- src/utils/stack/set/set.c
+++ src/utils/stack/set/set.c
@@ -1,1 +1,56 @@
 #include "set.h"
+
+#include "color_second_header.h"
+
+unsigned int* set_get_colors(struct set_t* set)
+{
+    return set->c;
+}
+
+struct set_t set_create(unsigned int c[NUM_COLORS])
+{
+    struct set_t s = {};
+    for (unsigned int index = 0; index < NUM_COLORS ; ++index)
+    {
+        s.c[index] = c[index];
+    }
+    return s;
+}
+
+int set_are_equals(struct set_t* s1, struct set_t* s2)
+{
+    for(int index = 0 ; index < NUM_COLORS; ++index)
+    {
+        if (s1->c[index] != s2->c[index])
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void set_display(struct set_t *set, char *prefix)
+{
+    printf("%s(", prefix);
+	for (enum color_t i = 0 ; i < NUM_COLORS ; ++i)
+	{
+		if (set->c[i] != 0)
+		{
+			printf("%s%s%s=%d (Q:%d)," ,color_prefix(i), color_to_short_string(i),CRESET, i, set->c[i]);	  
+		}
+	}
+  printf(")");
+}
+
+void set_short_display(struct set_t* set, char * prefix)
+{
+    printf("%s(", prefix);
+	for (enum color_t i = 0; i < NUM_COLORS; ++i)
+	{
+		if(set->c[i] != 0)
+		{	
+			printf("%s%s=%d%s", color_prefix(i), color_to_short_string(i), set->c[i], CRESET);
+		}
+	}
+	printf(")");
+}
--- src/utils/stack/set/set.h
+++ src/utils/stack/set/set.h
@@ -1,7 +1,10 @@
 #ifndef __SET_H__
 #define __SET_H__
 
+#include <stdio.h>
+
 #include "color.h"
+
 
 struct set_t {    
   unsigned int c[NUM_COLORS];
@@ -10,9 +13,13 @@ struct set_t {
 /*
     Display set
 */
-void set_display(struct set_t);
+void set_display(struct set_t* set, char * prefix);
+void set_short_display(struct set_t* set, char * prefix);
 
+unsigned int* set_get_colors(struct set_t* set);
 
-unsigned int* set_get_colors(struct set_t);
+struct set_t set_create(unsigned int c[NUM_COLORS]);
+
+int set_are_equals(struct set_t* s1, struct set_t* s2);
 
 #endif
