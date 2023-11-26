06762689456f2e1cc09c2cfb33e51dbf17857ac5
Nemo D'acremont <ndacremont@enseirb-matmeca.fr>
2023-11-26 15:51:55 +0100

    Start implementing masters_hand skill

---
+++ src/skills/skills_builders.c
@@ -0,0 +1,19 @@
+#include "skills_builders.h"
+#include "market.h"
+#include "set.h"
+
+void skill_masters_hand(struct player_t *player, struct market *market, struct builder_t *builder_bought)
+{
+	struct set_t provides = builder_provides(builder_bought);
+	unsigned int* colors_provides = set_get_colors(&provides);
+
+	struct available_tokens* available_tokens = get_available_tokens();
+}
+
+
+
+
+
+
+
+
