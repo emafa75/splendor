61b95ad27e5a411cde1cd85d30cf1a85cfc2058b
Martin Eyben <martin.eyben@enseirb-matmeca.fr>
2023-11-27 09:40:01 +0100

    add token_rob builder's skill

---
+++ src/skills/skills_builders.c
@@ -0,0 +1,15 @@
+#include "skills_builders.h"
+#include "market.h"
+
+int token_rob(struct player_t* robber_player, struct player_t* stolen_player, struct token_t* robbed_token)
+{
+    //check if the stollen_player has the desired token
+    if (market_is_in_market(&stolen_player->ressources.market, robbed_token))
+    {
+        //Move the robbed token in new inventory and remove it from the stolen player
+        market_pick_token(&stolen_player->ressources.market, robbed_token);
+        market_pay_token(&robber_player->ressources.market, robbed_token);
+        return 1;
+    }
+    return 0;
+}
--- src/skills/skills_builders.h
+++ src/skills/skills_builders.h
@@ -1,4 +1,13 @@
 #ifndef __SKILLS_BUILDERS_H__
 #define __SKILLS_BUILDERS_H__
 
+#include "players.h"
+#include "token.h"
+
+/*
+    Rob a specific token to a player
+    Returns 0 if impossible, -1 if success
+*/
+int token_rob(struct player_t* robber_player, struct player_t* stolen_player, struct token_t* robbed_token);
+
 #endif
