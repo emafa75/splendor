e036ec3e9ed94c66d056979ae2224b35974b87ff
Nemo D'acremont <ndacremont@enseirb-matmeca.fr>
2023-11-28 15:10:07 +0100

    Fix sets

--- src/utils/stack/stack.c
+++ src/utils/stack/stack.c
@@ -34,6 +34,12 @@ void * stack_pop(struct stack_t * stack)
 
 unsigned int stack_append(struct stack_t *stack, void *value)
 {
+	if (stack == NULL)
+	{
+		fprintf(stderr, "things dlkfdshjpifjpoiezjlooesn't w'");
+		return 0;
+	}
+
 	if (stack->head < MAX_STACK_SIZE - 1)
 	{
 		stack->head++;
