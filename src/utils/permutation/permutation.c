d50ae2dc5cd7c1f797af7565542a70b186f6f7c8
Martin Eyben <martin.eyben@enseirb-matmeca.fr>
2023-12-05 09:24:06 +0100

    indentation'

--- src/utils/permutation/permutation.c
+++ src/utils/permutation/permutation.c
@@ -4,28 +4,28 @@
 
 struct permutation_t identity()
 {
-    struct permutation_t permutation = {};
-    for (int index = 0; index < NUM_TOKENS; ++index)
-    {
-        permutation.permutation[index]=index;
-    }
-    return permutation;
+struct permutation_t permutation = {};
+for (int index = 0; index < NUM_TOKENS; ++index)
+{
+	permutation.permutation[index]=index;
+}
+return permutation;
 }
 
 struct permutation_t random_permutation(int random_seed)
 {
-    srand(random_seed);
-    struct permutation_t permutation = identity();
-    int rand_index = 0;
-    int tmp = 0;
-    //shuffle identity permutation
-    for (int index = 0; index < NUM_TOKENS; ++index)
-    {
-        rand_index = index + rand() % (NUM_TOKENS - index);
-        tmp = permutation.permutation[rand_index];
-        permutation.permutation[rand_index] = permutation.permutation[index];
-        permutation.permutation[index] = tmp;
-    }
-    return permutation;
+srand(random_seed);
+struct permutation_t permutation = identity();
+int rand_index = 0;
+int tmp = 0;
+//shuffle identity permutation
+for (int index = 0; index < NUM_TOKENS; ++index)
+{
+	rand_index = index + rand() % (NUM_TOKENS - index);
+	tmp = permutation.permutation[rand_index];
+	permutation.permutation[rand_index] = permutation.permutation[index];
+	permutation.permutation[index] = tmp;
+}
+return permutation;
 
 }
