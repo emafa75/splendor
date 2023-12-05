2445595eaf06849175cbead6efd473c00425e874
Nemo D'acremont <ndacremont@enseirb-matmeca.fr>
2023-12-05 14:42:55 +0100

    Create guild and market dir

--- src/cli/cli_utils.c
+++ src/cli/cli_utils.c
@@ -5,12 +5,12 @@ int getch(void)
 {
     struct termios oldattr, newattr;
     int ch;
-    tcgetattr( STDIN_FILENO, &oldattr );
+    tcgetattr(STDIN_FILENO, &oldattr);
     newattr = oldattr;
-    newattr.c_lflag &= ~( ICANON | ECHO );
-    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    newattr.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
     ch = getchar();
-    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
     return ch;
 }
 
---
+++ src/cli/guild/cli_guild.h
@@ -0,0 +1,11 @@
+
+#ifndef __CLI_GUILD_H__
+#define __CLI_GUILD_H__
+
+#include "guild.h"
+
+void display_global_guild();
+
+
+#endif
+
