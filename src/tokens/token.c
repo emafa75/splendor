fe36ceaf9f677da77a1c5ed08fd155755cb7bd41
Martin Eyben <martin.eyben@enseirb-matmeca.fr>
2023-12-08 15:42:03 +0100

    add random to the init token (except seed 0)

--- src/tokens/token.c
+++ src/tokens/token.c
@@ -1,9 +1,11 @@
 
 #include "token.h"
+#include "color.h"
 #include "game.h"
 #include "token_second_header.h"
 #include "skills.h"
 #include "set.h"
+#include <stdlib.h>
 
 static struct token_t tokens[NUM_TOKENS] = {};
 
@@ -51,34 +53,73 @@ void token_short_diplay(struct token_t t)
 
 void init_tokens(unsigned int seed)
 {
-	UNUSED(seed);
+	srand(seed);
 	int i = 0;
 	enum color_t color = 0;
 	enum color_t complex_token_colors[NUM_COLORS] = {};
-
-	while (i < NUM_TOKENS)
-	{
-		// Create first the complex tokens
-		if (i < NUM_COLORS)
+	/*
+		Reserve the seed 0 for default set
+	*/
+	if (seed == 0 ) {
+		while (i < NUM_TOKENS)
 		{
-			complex_token_colors[(color - 1) % NUM_COLORS] = 0;
-			complex_token_colors[color] = 2;
-			struct set_t set_for_complex_token = {};
-
-			for (int index = 0; index < NUM_COLORS; ++index)
+			// Create first the complex tokens
+			if (i < NUM_COLORS)
 			{
-				set_for_complex_token.c[index] = complex_token_colors[index];
+				complex_token_colors[(color - 1) % NUM_COLORS] = 0;
+				complex_token_colors[color] = 2;
+				struct set_t set_for_complex_token = {};
+
+				for (int index = 0; index < NUM_COLORS; ++index)
+				{
+					set_for_complex_token.c[index] = complex_token_colors[index];
+					++set_for_complex_token.num_colors;
+				}
+
+				tokens[i] = create_complex_token(set_for_complex_token);	 
+			}
+			else
+			{
+				tokens[i] = create_simple_token(color);
 			}
 
-			tokens[i] = create_complex_token(set_for_complex_token);	 
+			color = (color + 1) % NUM_COLORS;
+			++i;
 		}
-		else
+	}
+	else {
+		while (i < NUM_TOKENS)
 		{
-			tokens[i] = create_simple_token(color);
-		}
+			/*
+				Choose to have a complex or simple token randomly
+			*/
+			int is_complex = rand() % 2;
+			
+			if(is_complex)
+			{
+				struct set_t set_for_complex_token = {};
+				for (int index = 0; index < MAX_COLORS_PER_TOKENS; ++index)
+				{
+					/*
+						Choose a random color and add it to the set
+					*/
+					enum color_t rand_color = rand() % NUM_COLORS;
+					++set_for_complex_token.c[rand_color];
+					set_for_complex_token.num_colors += (set_for_complex_token.c[index] == 1);	
+				}
 
-		color = (color + 1) % NUM_COLORS;
-		++i;
+				tokens[i] = create_complex_token(set_for_complex_token);
+			}
+			else{
+				/*
+					Create simple token with a random color
+				*/
+				enum color_t rand_color = rand() % NUM_COLORS;
+				tokens[i] = create_simple_token(rand_color);
+
+			}
+			++i;
+		}
 	}
 }
 
