f63d085f68784fb85c517312d0f5def95e569efb
Nemo D'acremont <ndacremont@enseirb-matmeca.fr>
2023-12-08 16:49:53 +0100

    Little evaluator

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ CFLAGS := -O0 -Wall -Wextra -std=c99 -g3 $(MUNIFICENCE_FLAGS) # -fstack-usage
 
 PROJECT_TARGET_EXEC := project
 TEST_TARGET_EXEC := test
-
+EVALUATOR_TARGET_EXEC := evaluator
 
 BUILD_DIR := ./build
 SRC_DIRS := ./src
@@ -21,6 +21,7 @@ TST_DIRS := ./tst
 
 PROJECT_MAIN_FILE_NAME := ./src/project.c
 TEST_MAIN_FILE_NAME := ./tst/test.c
+EVALUATOR_MAIN_FILE_NAME := ./evaluator_src/main.c
 
 
 # Note the single quotes around the * expressions. The shell will incorrectly expand these otherwise, but we want to send the * directly to the find command.
@@ -33,6 +34,7 @@ TST_SRCS := $(shell find $(TST_DIRS) -name '*.c')
 # As an example, ./your_dir/hello.cpp turns into ./build/./your_dir/hello.cpp.o
 PROJECT_OBJS := $(PROJECT_MAIN_FILE_NAME:%=$(BUILD_DIR)/%.o) $(SRCS:%=$(BUILD_DIR)/%.o) 
 TEST_OBJS := $(SRCS:%=$(BUILD_DIR)/%.o) $(TST_SRCS:%=$(BUILD_DIR)/%.o)
+EVALUATOR_OBJS := $(EVALUATOR_MAIN_FILE_NAME:%=$(BUILD_DIR)/%.o) $(SRCS:%=$(BUILD_DIR)/%.o) 
 
 
 # String substitution (suffix version without %).
@@ -53,6 +55,7 @@ CFLAGS := $(CFLAGS) $(INC_FLAGS) # -MMD -MP
 
 all: project
 
+evaluator: $(BUILD_DIR)/$(EVALUATOR_TARGET_EXEC)
 
 project: $(BUILD_DIR)/$(PROJECT_TARGET_EXEC)
 
@@ -60,15 +63,22 @@ test: clean $(BUILD_DIR)/$(TEST_TARGET_EXEC)
 	./test
 
 
-# The final build step.
+# The final build step for project
 $(BUILD_DIR)/$(PROJECT_TARGET_EXEC): $(PROJECT_OBJS)
 	#Compiles the project
 	@$(CC) $(PROJECT_OBJS) -o $(PROJECT_TARGET_EXEC) $(LDFLAGS)  # $@
 
 
+# The final build step for tests
 $(BUILD_DIR)/$(TEST_TARGET_EXEC): $(TEST_OBJS)
 	#Compiles the tests
 	@$(CC) $(TEST_OBJS) -o $(TEST_TARGET_EXEC) $(LDFLAGS)  # $@
+
+
+# The final build step for evaluator
+$(BUILD_DIR)/$(EVALUATOR_TARGET_EXEC): $(EVALUATOR_OBJS)
+	#Compiles the evaluator
+	$(CC) $(EVALUATOR_OBJS) -o $(EVALUATOR_TARGET_EXEC) $(LDFLAGS)  # $@
 
 
 clang_custom_lib_support:
@@ -92,7 +102,9 @@ color:
 clean:
 	touch $(BUILD_DIR)/avoid_error
 	rm -r $(BUILD_DIR)/*
-	rm -f $(PROJECT_TARGET_EXEC) $(TEST_TARGET_EXEC)
+	rm -f $(PROJECT_TARGET_EXEC) $(TEST_TARGET_EXEC) $(EVALUATOR_TARGET_EXEC)
+
+
 
 # Include the .d makefiles. The - at the front suppresses the errors of missing
 # Makefiles. Initially, all the .d files will be missing, and we don't want those
---
+++ evaluator_src/main.c
@@ -0,0 +1,191 @@
+/**
+ *
+ * @filename main.c
+ * @date 2023-11-07 14:14
+ * @author Nemo D'ACREMONT <nemo.dacremont@enseirb-matmeca.fr>, Martin EYBEN <martin.eyben@enseirb-matmeca.fr>
+ * @brief ...
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <getopt.h>
+
+#include "color.h"
+#include "permutation.h"
+#include "players.h"
+#include "guild.h"
+#include "market.h"
+#include "builder.h"
+#include "skills.h"
+#include "token.h"
+#include "can_buy.h"
+#include "game.h"
+
+
+#define _NB_MIN_PARAMS_ 1
+
+#ifndef PRINT	
+	#define PRINT 1
+#endif
+
+enum parameters {
+	MAX_TURNS = 10,
+	POINTS_TO_WIN = 10,
+	RANDOM_SEED = 0,
+	BUILDER_SEEED = 0,
+	MARKET_SEED = 0,
+};
+
+void print_usage(char *argv[]);
+
+/*
+ *	Display the stats for evaluator
+ */
+void print_stats_header(FILE* file);
+void display_stats(struct game_statistics stats, struct game_parameters game_parameters, FILE* file);
+
+
+/*
+	Display options for the game
+*/
+void display_options();
+
+/*
+	Init all parameters 
+*/
+
+struct game_parameters game_params = {
+	.points_to_win = POINTS_TO_WIN,
+	.max_turns = MAX_TURNS,
+	.market_seed = MARKET_SEED,
+	.builder_seed = BUILDER_SEEED,
+	.random_seed = RANDOM_SEED,
+};
+
+int main(int argc, char *argv[])
+{
+	
+	if (argc < _NB_MIN_PARAMS_)
+	{
+		print_usage(argv);
+		return EXIT_FAILURE;
+	}
+	
+	/*
+		Get optionnal parameters 
+	*/
+	int options;
+
+	while ((options = getopt(argc,argv, "s:m:c:p:t:")) != -1)
+	{
+		switch (options) {
+			case 's':
+				game_params.random_seed = atoi(optarg);
+				break;
+			case 'm':
+				game_params.max_turns = MIN(atoi(optarg), MAX_MAX_TURNS);
+				break;
+			case 'c':
+				game_params.builder_seed = atoi(optarg);
+				break;
+			case 'p':
+				game_params.points_to_win = atoi(optarg);
+				break;
+			case 't':
+				game_params.market_seed = atoi(optarg);
+				break;
+			default: 
+				print_usage(argv);
+				return EXIT_FAILURE;
+		}
+	}
+
+	// display_options();
+	print_stats_header(stdout);
+
+	int n = 10;
+
+	for (int r_seed = 0 ; r_seed < n ; ++r_seed)
+	{
+		for (int b_seed = 0 ; b_seed < n ; ++b_seed)
+		{
+			for (int t_seed = 0 ; t_seed < n ; ++t_seed)
+			{
+				game_params.random_seed = r_seed;
+				game_params.builder_seed = b_seed;
+				game_params.market_seed = t_seed;
+				/*
+					Init all instances
+				*/
+				struct game_t game = {};
+				init_game(&game, game_params);
+
+				/*
+					Play the game
+				*/
+				struct game_statistics game_stats = game_play(&game, PRINT);
+
+				/*
+					End of the game, print results 
+				*/
+				struct turn_t* last_turn = game_get_current_turn(&game);
+				int winner = get_winner(last_turn);
+
+
+				display_stats(game_stats, game_params, stdout);
+			}
+		}
+	}
+	return EXIT_SUCCESS;
+}
+
+
+void print_stats_header(FILE* file)
+{
+	fprintf(file, "random_seed;seed_builders;seed_token;choices;used_favor;used_skill;num_picked_tokens;forces_skip;nb_turns\n");
+}
+
+
+void display_stats(struct game_statistics stats, struct game_parameters game_parameters, FILE* file)
+{
+	fprintf(file, "%d;%d;%d;",
+			game_parameters.random_seed,
+			game_parameters.builder_seed,
+			game_parameters.market_seed);
+
+
+	for (int i = 0 ; i < NUM_CHOICE - 1 ; ++i)
+		fprintf(file, "%d,", stats.choices[i]);
+
+	fprintf(file, "%d;", stats.choices[NUM_CHOICE - 1]);
+
+	fprintf(file, "%d;%d;%d;%d;%d\n",
+			stats.used_favor,
+			stats.used_skill,
+			stats.num_picked_tokens,
+			stats.forced_skip,
+			stats.nb_turns
+			);
+
+}
+
+
+void display_options()
+{
+	printf("Random seed : %d\nBuilder seed : %d\nMarket seed : %d\nPoints to win a game : %d\nMax turns : %d\n", 
+		game_params.random_seed,
+		game_params.builder_seed,
+		game_params.market_seed,
+		game_params.points_to_win,
+		game_params.max_turns
+	);
+}
+
+
+void print_usage(char *argv[])
+{
+	fprintf(stderr, "Usage: %s [-s random_seed] [-m max_turns] [-c builder_seed] [-t token seed] [-p points_to_win]\n", argv[0]);
+	return;
+}
