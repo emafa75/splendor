4959199ec371ad6f7e477589fec9783978a0ef05
Martin Eyben <martin.eyben@enseirb-matmeca.fr>
2023-12-17 22:26:27 +0100

    add strlen that doesn't count the special chars

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,14 +1,15 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
+#include <string.h>
 
 #define UNUSED(x) (void)(x)
 
 #define MIN(__x, __y) \
-  ((__x) < (__y) ? (__x) : (__y))
+((__x) < (__y) ? (__x) : (__y))
 
 #define MAX(__x, __y) \
-  ((__x) > (__y) ? (__x) : (__y))
+((__x) > (__y) ? (__x) : (__y))
 
 /*
 	Used to use a display function only if _x is true
@@ -17,5 +18,6 @@
 	(_x) ? (_y) : UNUSED(_x);
 
 
+size_t str_len_special(const char* string);
 
 #endif
