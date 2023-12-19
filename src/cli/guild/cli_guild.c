2249a1d0b2376e8f3feaddd082bb45084486767e
Martin Eyben <martin.eyben@enseirb-matmeca.fr>
2023-12-19 11:12:26 +0100

    fix Nemo betise

--- src/cli/guild/cli_guild.c
+++ src/cli/guild/cli_guild.c
@@ -1,6 +1,7 @@
 
 #include "cli_guild.h"
 #include "builder.h"
+#include "cli_builders.h"
 #include "cli_utils.h"
 #include "guild.h"
 #include "vector2.h"
@@ -12,26 +13,29 @@ struct vector2_t display_global_guild(struct vector2_t position, struct guild_t*
 	char text[100] = {};
 
 	// Nb dispo
-	sprintf(text, "builders disponibles: %d", guild_nb_builder(guild));
-	printToCoordinates(position.y, position.x, text);
-
-
-	// Nb dispo par stack
-	sprintf(text, "builders disponibles: %d", guild_nb_builder(guild));
-	printToCoordinates(position.y, position.x, text);
-}
-
-
-void display_global_guild_stacks(struct vector2_t position, struct guild_t* guild, struct vector2_t dimension)
-{
-	char text[100] = {};
 	
-	for (int i = 0 ; i < NUM_LEVELS ; ++i)
+	/*
+		Print available builders
+	*/
+	struct available_builders* available_builders = guild_get_available_builders(guild);
+	struct builder_t* builder;
+
+	sprintf(text, BOLD "Game guild (%d) : " CRESET, available_builders->n_builders_available);
+	printToCoordinates(position.x, position.y, text);
+	position = vector2_add(position, vector2_down());
+
+	for (int index = 0; index < MAX_BUILDERS; ++index)
 	{
-		
-		sprintf(text, "niv - %d: %d dispo", i + 1, guild_get_stack(guild, i));
-		printToCoordinates(position.y, position.x, "niv");
+		builder = available_builders->builders[index];
+		if( builder != NULL)
+		{
+			cli_builder_display(position, builder, " --- ");
+			position = vector2_add(position, vector2_down());
+		}
 	}
+
+	return position;
+	
 }
 
 
